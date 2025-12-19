b0649850f6796ee536a6845f9bb0400b33fd8bfa
Anmol Verma <vermaanmol1223@gmail.com>
2025-12-20 00:20:39 +0530

    Update Client.js

--- backend/models/Client.js
+++ backend/models/Client.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-
-const clientSchema = new mongoose.Schema({
+const clientSchema = new mongoose.Schema
+  ({
   name: String,
   designation: String,
   description: String,
   image: String
 });
-
 module.exports = mongoose.model("Client", clientSchema);
