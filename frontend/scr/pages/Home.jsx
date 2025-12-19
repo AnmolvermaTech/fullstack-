b20862cef8bed71eda89e1689f8a6d3d14140bbd
Anmol Verma <vermaanmol1223@gmail.com>
2025-12-20 00:32:46 +0530

    Create Home.jsx

---
+++ frontend/scr/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Projects from "../components/Projects";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Landing Page</h1>
+      <Projects />
+    </div>
+  );
+}
